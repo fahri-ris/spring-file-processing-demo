fe405d62c8cea20b3f9db3fd925bbbbe21f3333f
Fahri Ris Ramdhani <fahri.ramdhani@tujuhsembilan.com>
2024-04-18 15:13:40 +0700

    file processing upload and get direct to db mysql

---
+++ src/main/java/com/demo/fileprocessing/SpringFileProcessingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.fileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileProcessingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileProcessingDemoApplication.class, args);
+	}
+
+}
